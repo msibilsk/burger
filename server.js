8fd0361a77ba07b72a3a1dc6179364d7aa108d62
Mary Willis <msibilsk@gmail.com>
2017-02-14 16:08:13 -0600

    trying clearDB

--- server.js
+++ server.js
@@ -26,17 +26,3 @@ var routes = require("./controllers/burgers_controller.js");
 app.use("/", routes);
 
 app.listen(port);
-
-var pg = require('pg');
-
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-});
